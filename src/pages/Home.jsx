26119527845c32f4a79b55bc5ca80f36464b9347
HarshadaGirase <harshadajg2001@gmail.com>
2024-12-11 14:37:46 +0530

    added imagecomponent

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import Header from '../components/common/Header';
 import HeroSection from '../components/HeroSection.jsx';
 import Mission from '../components/Mission.jsx';
 import PerspectiveSection from '../components/perspectivesection.jsx';
+import ImageSection from '../components/ImageSection';
 function Home(){
     return (
         <div className='flex flex-col gap-10 m-8'>
             <Header />
             <HeroSection/>
             <Mission/>     
-            <PerspectiveSection/>      
+            <PerspectiveSection/>    
+            <ImageSection/>  
         </div>
     );
 }

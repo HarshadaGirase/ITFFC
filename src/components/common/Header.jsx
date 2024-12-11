57998554b88153fc7dcbf2c9dfe78ae4b0eedf11
HarshadaGirase <harshadajg2001@gmail.com>
2024-12-11 18:47:01 +0530

    fixed header logo size

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -17,20 +17,20 @@ function Header() {
   }, [isMenuOpen]);
 
   return (
-    <header className="relative fixed top-0  w-full bg-white xl:shadow-md z-50 justify-center  font-roboto  ">
+    <header className="relative fixed top-0  w-full bg-white xl:shadow-md z-50 justify-center   ">
       <div className="flex items-center justify-between px-0 sm:px-0 md:px-12    h-18 sm:h-20 lg:h-30">
         {/* Logo */}
-       <div className="  rounded-full flex justify-center items-center">
+       <div className="  rounded-full flex justify-center items-center p-2">
         <img
           src={logo1} // Replace with the correct image path
           alt="ITFFC Logo"
-          className="w-20 h-18 sm:h-18 md:h-20 lg:h-30 object-contain p-2"
+          className="w-[38px] h-[38px] sm:h-[38px] sm:w-[38px] md:h-[52.05px] md:w-[53.11px] lg:h-[64.28px] lg:w-[65.59px] object-contain p-2"
         />
-        <div className="p-2 h-[55px] border-l-2 border-black sm:h-[55px] md:h-[55px] lg:h-[55px]"></div>
+        <div className="p-2 h-[22px] border-l-2 border-black sm:h-[55px] md:h-[55px] lg:h-[35px]"></div>
       {/* Text Section */}
       <div>
-        <h1 className="text-xl font-bold text-blue-900  text-[15px] sm:text-[15px] lg:text-[20px]">ITFFC</h1>
-        <p className="text-xs text-gray-900 text-">Innocent Times Foundation For Children</p>
+        <h1 className="text-[15px] leading-[19px] sm:text-[15px] sm:leading-[19px] md:text-[15px] md:leading-[19px] lg:text-[21px] lg:leading-[26px] font-bold text-[#304598]  font-quicksand ">ITFFC</h1>
+        <p className="text-[4.75px] leading-[6px] sm:text-[4.75px] sm:leading-[6px] md:text-[6.51px] md:leading-[8.13px] lg:text-[8.03px] lg:leading-[10.04px]  text-black font-inter">Innocent Times Foundation</p>  <p className="text-[4.75px] leading-[6px] sm:text-[4.75px] sm:leading-[6px] md:text-[6.51px] md:leading-[8.13px] lg:text-[8.03px] lg:leading-[10.04px]  text-black font-inter" > For Children</p>
       </div>
 
       </div>
@@ -80,9 +80,9 @@ function Header() {
         <nav
           className={`${
             isMenuOpen ? "block" : "hidden"
-          } absolute sm:static top-[80px] sm:top-auto left-0 w-full sm:w-auto bg-white sm:bg-transparent  sm:flex items-center flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-6 px-4 sm:px-0 py-6 sm:py-0 `}
+          } absolute sm:static top-[60px] sm:top-auto left-0 w-full sm:w-auto bg-white sm:bg-transparent  sm:flex items-center flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-6 px-4 sm:px-0 py-6 sm:py-0 `}
         >
-          <ul className="flex flex-col sm:flex-row space-y-8 sm:space-y-0 sm:space-x-6 text-[15px] sm:text-[12px] md:text-[13px] lg:text-[16px] text-black font-sans ">
+          <ul className="flex flex-col sm:flex-row space-y-8 sm:space-y-0 sm:space-x-6 text-[15px] sm:text-[12px] md:text-[13px] lg:text-[16px] text-black font-quicksand font-semibold">
             <li>
               <a
                 href="#home"

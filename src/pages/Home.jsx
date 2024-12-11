665ebb4b4c4fe8aa688255bdbd3d2c497beaf8a7
HarshadaGirase <harshadajg2001@gmail.com>
2024-12-11 10:54:42 +0530

    added missiossection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/common/Header';
 import HeroSection from '../components/HeroSection.jsx';
+import Mission from '../components/Mission.jsx';
+
 function Home(){
     return (
-        <div className='flex flex-col gap-4 m-8'>
+        <div className='flex flex-col gap-10 m-8'>
             <Header />
             <HeroSection/>
-           
+            <Mission/>           
         </div>
     );
 }

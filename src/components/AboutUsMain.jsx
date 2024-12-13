1650b3608e63379ac188f3a931169e14d808f6da
HarshadaGirase <harshadajg2001@gmail.com>
2024-12-13 21:34:57 +0530

    WhoWeAre component added

--- src/components/AboutUsMain.jsx
+++ src/components/AboutUsMain.jsx
@@ -1,10 +1,10 @@
 import AboutHeroSection from '../components/AboutUs/AboutHeroSection.jsx';
-
+import WhoWeAre from '../components/AboutUs/WhoWeAre.jsx';
 function AboutUsMain() {
   return (
     <div className='flex flex-col gap-10 m-4 sm:m-4 md:m-8 lg:m-8'>
          <AboutHeroSection/>
-        
+        <WhoWeAre/>
     </div>
   )
 }

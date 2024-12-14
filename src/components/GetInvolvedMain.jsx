98e406933bfd0a3a88a32e70f46c67c742c16d0b
HarshadaGirase <harshadajg2001@gmail.com>
2024-12-14 14:22:05 +0530

    added GetInvolvedHelp component

--- src/components/GetInvolvedMain.jsx
+++ src/components/GetInvolvedMain.jsx
@@ -1,11 +1,13 @@
 
 import GetInvolvedHeroSection from '../components/GetInvolved/GetInvolvedHeroSection.jsx';
 import CommunityInitiatives from '../components/GetInvolved/CommunityInitiatives.jsx';
+import GetInvolvedHelp from '../components/GetInvolved/GetInvolvedHelp.jsx';
 function GetInvolvedMain() {
     return (
       <div className='flex flex-col gap-10 m-4 sm:m-4 md:m-8 lg:m-8'>
           <GetInvolvedHeroSection/>
           <CommunityInitiatives/>
+          <GetInvolvedHelp/>
       </div>
     )
   }

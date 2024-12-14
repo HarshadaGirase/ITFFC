e33ce634de423b9c4f6be066243e6f10e0ff406e
HarshadaGirase <harshadajg2001@gmail.com>
2024-12-14 15:46:50 +0530

    added donate herosection

---
+++ src/components/DonateMain.jsx
@@ -0,0 +1,10 @@
+import DonateHeroSection from '../components/Donate/DonateHeroSection.jsx';
+
+function AboutUsMain() {
+  return (
+    <div className='flex flex-col gap-10 m-4 sm:m-4 md:m-8 lg:m-8'>
+       <DonateHeroSection/>
+    </div>
+  )
+}
+export default AboutUsMain;
